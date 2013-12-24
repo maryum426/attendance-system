1cdbd42e865647a86c248ded6163e5b0a20d89fc
unknown <maryum.babar@KL-03.kualitatem.local>
2013-12-24 14:01:59 +0500

    2- phonegap7 24

--- www/js/attendance.js
+++ www/js/attendance.js
@@ -70,7 +70,7 @@ var department;
                 var files = e.target.files || e.dataTransfer.files;
                 file = files[0];
                 $("#my_image").css({'display':'block'});
-                $("#my_image").attr("src",URL.createObjectURL(file));
+                //$("#my_image").attr("src",URL.createObjectURL(file));
                  alert("In change event1");
                 //var parseFile = new Parse.File("mypic.jpg", {base64:file});
                 
