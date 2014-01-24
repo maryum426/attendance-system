4f2f7592317fec022f5be3c8db7f1f73853b4b15
unknown <maryum.babar@KL-03.kualitatem.local>
2014-01-24 11:59:19 +0500

    1- shift submit4 24

--- www/js/attendance.js
+++ www/js/attendance.js
@@ -1725,8 +1725,8 @@ var table = '<table style="border:1px solid #000;text-align: center;border-colla
                                 results[0].save(null, {
                                     success:function (kuali) {
                                         console.log(kuali + " saved successfully");
-                                        $.when(syncDataCheckIn()).done(
-                                        setTimeout(function(){window.location = "index.html";},5000));
+                                        syncDataCheckIn();
+                                        setTimeout(function(){window.location = "index.html";},5000);
                                         
                                           
                                     },
@@ -1766,8 +1766,8 @@ var table = '<table style="border:1px solid #000;text-align: center;border-colla
                                 results[0].save(null, {
                                     success:function (kuali) {
                                         console.log(kuali + " saved successfully");
-                                        $.when(syncDataCheckIn()).done(
-                                        setTimeout(function(){window.location = "index.html";},5000));
+                                        syncDataCheckIn(); 
+                                        setTimeout(function(){window.location = "index.html";},5000);
                                     },
                                     error:function (pSweet, error) {
                                         console.log("saveRecord() -> " + error.code + " " + error.message);
@@ -1832,8 +1832,8 @@ var table = '<table style="border:1px solid #000;text-align: center;border-colla
                                         results[0].save(null, {
                                             success:function (virtualf) {
                                                 console.log(virtualf + " saved successfully");
-                                                $.when(syncDataCheckIn()).done(deleteLocalvf(userpin));
-                                                
+                                                syncDataCheckIn();  
+                                                deleteLocalvf(userpin);
 
                                             },
                                             error:function (pSweet, error) {
@@ -1893,7 +1893,8 @@ var table = '<table style="border:1px solid #000;text-align: center;border-colla
                                             results[0].save(null, {
                                                 success:function (kuali) {
                                                     console.log(kuali + " saved successfully");
-                                                    $.when(syncDataCheckIn()).done(deleteLocalkm(userpin));
+                                                    syncDataCheckIn();  
+                                                    deleteLocalkm(userpin);
                                                 },
                                                 error:function (pSweet, error) {
                                                     console.log("saveRecord() -> " + error.code + " " + error.message);
