c6b36bacf32c2bd43b2f30bfcdb6acd86265fddc
unknown <maryum.babar@KL-03.kualitatem.local>
2014-01-28 13:51:18 +0500

    3- convertimage2 28

--- www/js/attendance.js
+++ www/js/attendance.js
@@ -6,6 +6,7 @@ var department;
 var userAvatar = null;
 var offlinePic  = null;
 var offPicData = null;
+var syncImage = null;
 var picurl;
 var db = window.openDatabase("users", "1.0", "Users", 2048576);
 var table = '<table style="border:1px solid #000;text-align: center;border-collapse:collapse;margin-top:10px;margin-bottom:20px;">';
@@ -395,6 +396,10 @@ var temp_username, temp_userpin, temp_department, temp_company, temp_userAvatar,
                 }
 
                 context.drawImage(image, offsetX, offsetY, imageWidth, imageHeight);
+                canvas.toBlob(function(blob){
+                    syncImage = blob;
+                    console.log("Image Converted to Blob!");
+                },'image/jpeg');
                 var data2 = canvas.toDataURL('image/jpeg');
 
                 data2 = data2.replace(/^data:image\/(png|jpeg);base64,/, "");
@@ -473,18 +478,53 @@ var temp_username, temp_userpin, temp_department, temp_company, temp_userAvatar,
             //console.log("Pic3: "+pic3);
             //console.log("PicType: " + pic3.type);
             
-            var binImg = atob(pic3);
-            var length = binImg.length;
-            var ab = new ArrayBuffer(length);
-            var dv = new DataView(ab);
-            var ua = new Uint8Array(ab);
-            for (var i = 0; i < length; i++) {
-                ua[i] = binImg.charCodeAt(i);
-            }
-            console.log("Creating Blob1!");
-            var blob = new Blob([dv],{type:"image/jpeg"});
+            var thumbnail = 400;
+            var ppWidth, ppHeight;
+            //console.log("Image: " + data);
+            var image = new Image();
+            image.src = pic3;
+            
+            //Resizing Image
+            var canvas1 = document.createElement('canvas');
 
-            var userAvatar = 'https://api.parse.com/1/files/' + 'mypicLocal.jpg';
+            canvas1.width = thumbnail;
+            canvas1.height = thumbnail;
+
+
+            image.onload = function(){
+                
+                ppWidth = image.width;
+                ppHeight = image.height;
+
+                var context = canvas1.getContext('2d');
+                context.clearRect(0, 0, thumbnail, thumbnail);
+                var imageWidth;
+                var imageHeight;
+                var offsetX = 0;
+                var offsetY = 0;
+
+
+
+                if (image.width > image.height) {
+                    imageWidth = Math.round(thumbnail * image.width / image.height);
+                    imageHeight = thumbnail;
+                    offsetX = - Math.round((imageWidth - thumbnail) / 2);
+                    //console.log("IF");
+                } else {
+                    imageHeight = Math.round(thumbnail * image.height / image.width);
+                    imageWidth = thumbnail;    
+                    offsetY = - Math.round((imageHeight - thumbnail) / 2);            
+                    //console.log("ELSE");
+                }
+
+                context.drawImage(image, offsetX, offsetY, imageWidth, imageHeight);
+                canvas1.toBlob(function(blob){
+                    syncImage = blob;
+                    console.log("Image Converted to Blob!");
+                },'image/jpeg');
+            };
+            
+            userAvatar = 'https://api.parse.com/1/files/' + 'mypicLocal.jpg';
             
              $.ajax({
                     type: "POST",
@@ -1715,14 +1755,14 @@ var temp_username, temp_userpin, temp_department, temp_company, temp_userAvatar,
                 
                     if (company == 'virtualforce'){
                         
-                            window.localStorage.setItem("picurl",offPicData);
-                            upLocalvfIn(offPicData,currentTime,department,status,userpin);
+                            window.localStorage.setItem("picurl",offlinePic);
+                            upLocalvfIn(offlinePic,currentTime,department,status,userpin);
                             
                     }
                     else if (company == 'kualitatem'){
 
-                            window.localStorage.setItem("picurl",offPicData);
-                            upLocalkmIn(offPicData,currentTime,department,status,userpin);
+                            window.localStorage.setItem("picurl",offlinePic);
+                            upLocalkmIn(offlinePic,currentTime,department,status,userpin);
 
                     }
                 
